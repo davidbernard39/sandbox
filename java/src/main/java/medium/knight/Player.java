37551e1a522e917e0eb8052050cfe50cf28ca137
David Bernard <david.bernard39@gmail.com>
2018-01-04 14:19:38 +0100

    solve problem shadows of knight

--- java/src/main/java/medium/knight/Player.java
+++ java/src/main/java/medium/knight/Player.java
@@ -13,23 +13,68 @@ class Player {
         static final String LINE_SEPARATOR = "\n";
         static final String EMPTY_POSITION = ".";
         static final String KNIGHT_POSITION = "K";
-        public static final String EMPTY_STRING = "";
+        static final String EMPTY_STRING = "";
+
+        static final String UP = "U";
+        static final String DOWN = "D";
+        static final String LEFT = "L";
+        static final String RIGHT = "R";
 
         private int width;
         private int height;
         private int knightLine;
         private int knightColumn;
 
+        private int maxLine;
+        private int minLine;
+        private int maxColumn;
+        private int minColumn;
 
         public Board(int width, int height) {
             super();
             this.width = width;
             this.height = height;
+            this.minColumn = 0;
+            this.maxColumn = width;
+            this.minLine = 0;
+            this.maxLine = height;
         }
 
         public void knightAt(int knightLine, int knightColumn) {
             this.knightLine = knightLine;
             this.knightColumn = knightColumn;
+        }
+
+
+        public String move(String direction) {
+            int line = moveLine(direction);
+            int column = moveColumn(direction);
+            knightAt(line, column);
+            return column + " " + line;
+        }
+
+        private int moveColumn(String direction) {
+            int column = knightColumn;
+            if (direction.contains(LEFT)) {
+                maxColumn = knightColumn;
+                column = minColumn + ((knightColumn - minColumn) / 2);
+            } else if (direction.contains(RIGHT)) {
+                minColumn = knightColumn;
+                column = knightColumn + ((maxColumn - knightColumn) / 2);
+            }
+            return column;
+        }
+
+        private int moveLine(String direction) {
+            int line = knightLine;
+            if (direction.contains(UP)) {
+                maxLine = knightLine;
+                line = minLine + ((knightLine - minLine) / 2);
+            } else if (direction.contains(DOWN)) {
+                minLine = knightLine;
+                line = knightLine + ((maxLine - knightLine) / 2);
+            }
+            return line;
         }
 
         @Override
@@ -61,6 +106,9 @@ class Player {
         int X0 = in.nextInt();
         int Y0 = in.nextInt();
 
+        Board board = new Board(W, H);
+        board.knightAt(Y0, X0);
+
         // game loop
         while (true) {
             String bombDir = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
@@ -70,7 +118,7 @@ class Player {
 
 
             // the location of the next window Batman should jump to.
-            System.out.println("0 0");
+            System.out.println(board.move(bombDir));
         }
     }
 }
--- java/src/test/java/medium/knight/PlayerTest.java
+++ java/src/test/java/medium/knight/PlayerTest.java
@@ -1,5 +1,6 @@
 package medium.knight;
 
+import medium.knight.Player.Board;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +10,7 @@ public class PlayerTest {
     @Test
     public void should_board_position_knight_on_correct_line() {
         // Given
-        Player.Board board = new Player.Board(1, 2);
+        Board board = new Board(1, 2);
 
         // When
         board.knightAt(1,0);
@@ -21,7 +22,7 @@ public class PlayerTest {
     @Test
     public void should_board_position_knight_on_correct_column() {
         // Given
-        Player.Board board = new Player.Board(2, 1);
+        Board board = new Board(2, 1);
 
         // When
         board.knightAt(0,1);
@@ -33,7 +34,7 @@ public class PlayerTest {
     @Test
     public void should_board_position_knight_on_correct_line_and_column() {
         // Given
-        Player.Board board = new Player.Board(3, 3);
+        Board board = new Board(3, 3);
 
         // When
         board.knightAt(1,1);
@@ -42,4 +43,132 @@ public class PlayerTest {
         assertThat(board.toString()).isEqualTo("...\n.K.\n...");
     }
 
+    @Test
+    public void should_move_up_to_upper_case_when_one_position_upper() {
+        Board board = new Board(1, 2);
+        board.knightAt(1,0);
+        assertThat(board.move("U")).isEqualTo("0 0");
+    }
+
+    @Test
+    public void should_move_up_to_case_up_when_two_position_upper() {
+        Board board = new Board(1, 3);
+        board.knightAt(2,0);
+        assertThat(board.move("U")).isEqualTo("0 1");
+    }
+
+    @Test
+    public void should_move_up_to_case_up_when_three_position_upper() {
+        Board board = new Board(1, 4);
+        board.knightAt(3,0);
+        assertThat(board.move("U")).isEqualTo("0 1");
+    }
+
+    @Test
+    public void should_move_up_to_case_up_when_four_position_upper() {
+        Board board = new Board(1, 5);
+        board.knightAt(4,0);
+        assertThat(board.move("U")).isEqualTo("0 2");
+    }
+
+    @Test
+    public void should_move_down_when_one_position_down() {
+        Board board = new Board(1, 2);
+        board.knightAt(0,0);
+        assertThat(board.move("D")).isEqualTo("0 1");
+    }
+
+    @Test
+    public void should_move_down_when_three_position_down() {
+        Board board = new Board(1, 4);
+        board.knightAt(0,0);
+        assertThat(board.move("D")).isEqualTo("0 2");
+    }
+
+    @Test
+    public void should_move_down_when_knight_in_the_middle() {
+        Board board = new Board(1, 7);
+        board.knightAt(3,0);
+        assertThat(board.move("D")).isEqualTo("0 5");
+    }
+
+    @Test
+    public void should_move_left_when_one_position_left() {
+        Board board = new Board(2, 1);
+        board.knightAt(0,1);
+        assertThat(board.move("L")).isEqualTo("0 0");
+    }
+
+    @Test
+    public void should_move_left_when_two_position_left() {
+        Board board = new Board(3, 1);
+        board.knightAt(0,2);
+        assertThat(board.move("L")).isEqualTo("1 0");
+    }
+
+    @Test
+    public void should_move_right_when_one_position_right() {
+        Board board = new Board(2, 1);
+        board.knightAt(0,0);
+        assertThat(board.move("R")).isEqualTo("1 0");
+    }
+
+    @Test
+    public void should_move_right_when_knight_in_the_middle() {
+        Board board = new Board(7, 1);
+        board.knightAt(0,3);
+        assertThat(board.move("R")).isEqualTo("5 0");
+    }
+
+    @Test
+    public void should_move_up_right_change_knight_position() {
+        Board board = new Board(2, 2);
+        board.knightAt(1,0);
+        board.move("UR");
+        assertThat(board.toString()).isEqualTo(".K\n..");
+    }
+
+    @Test
+    public void should_move_down_then_up() {
+        Board board = new Board(1, 6);
+        board.knightAt(0,0);
+        board.move("D");
+        board.move("U");
+        assertThat(board.move("D")).isEqualTo("0 2");
+    }
+
+    @Test
+    public void should_move_up_then_down() {
+        Board board = new Board(1, 8);
+        board.knightAt(7,0);
+        board.move("U");
+        board.move("D");
+        assertThat(board.move("U")).isEqualTo("0 4");
+    }
+
+    @Test
+    public void should_move_right_then_left() {
+        Board board = new Board(6, 1);
+        board.knightAt(0,0);
+        board.move("R");
+        board.move("L");
+        assertThat(board.move("R")).isEqualTo("2 0");
+    }
+
+    @Test
+    public void should_move_left_then_right() {
+        Board board = new Board(8, 1);
+        board.knightAt(0,7);
+        System.out.println(board);
+        System.out.println();
+        board.move("L");
+        System.out.println(board);
+        System.out.println();
+        board.move("R");
+        System.out.println(board);
+        System.out.println();
+        assertThat(board.move("L")).isEqualTo("4 0");
+        System.out.println(board);
+        System.out.println();
+    }
 }
